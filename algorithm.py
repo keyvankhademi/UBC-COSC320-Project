ef4b480672053ef73b3ae0276fc3663b0922f93e
keyvankhademi <keyvankhademi@gmail.com>
2021-03-25 21:33:49 -0700

    Add plot

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,18 @@
+import time
 
 
 def naive_fix(text, shorties):
     tokens = text.split()
     result = [shorties[x] if x in shorties else x for x in tokens]
     return " ".join(result)
+
+
+def fix_abbreviations(tweets, shorties):
+    return [naive_fix(x, shorties) for x in tweets]
+
+
+def time_fix_abbreviations(tweets, shorties):
+    start = time.time()
+    fix_abbreviations(tweets, shorties)
+    end = time.time()
+    return end - start
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from time import sleep
-
 import pandas as pd
-
-from algorithm import naive_fix
+from algorithm import time_fix_abbreviations
+import matplotlib.pyplot as plt
 
 abbreviations = pd.read_csv("data/abbreviations.csv")
 contractions = pd.read_csv("data/contractions.csv")
@@ -10,22 +8,26 @@ tweets = pd.read_csv("data/tweets.csv")
 
 contractions = contractions.rename(columns={"Contraction": "Abbreviations", "Meaning": "Text"})
 shorties = pd.concat([abbreviations, contractions])
-
-tweets = tweets[:100]
-
 shorties = {x[0]: x[1] for i, x in shorties.iterrows()}
+
+tweets = tweets[:100000]
 tweets = [x[5] for i, x in tweets.iterrows()]
-fixed_tweets = [naive_fix(x, shorties) for x in tweets]
 
-for i in range(len(tweets)):
+# fixed_tweets = [naive_fix(x, shorties) for x in tweets]
 
-    f = True
-    for a, b in shorties.items():
-        if a in tweets[i].split():
-            f = False
-    if f:
-        continue
+time_data_x = []
+time_data_y = []
 
-    print("=====")
-    print(tweets[i])
-    print(fixed_tweets[i])
+for i in range(0, 100000, 1000):
+    print(i)
+    # df = tweets[:i]
+    # tweets_list = [x[5] for j, x in df.iterrows()]
+    time_data_x.append(i)
+    time_data_y.append(time_fix_abbreviations(tweets[:i], shorties))
+
+
+plt.plot(time_data_x, time_data_y)
+plt.xlabel("n (number of tweets)")
+plt.ylabel("t (execution time in seconds)")
+plt.title("Execution Time of Naive Algorithm")
+plt.show()
